3aeafe9677cb766b51c4f128fb6cf858bef1933c
MaagMich <maag.michael94@web.de>
2021-11-09 12:45:16 +0100

    Changed AccelToPos due to misunderstandings with  GitKranken

--- Code/Application/Application/AccelToPos.h
+++ Code/Application/Application/AccelToPos.h
@@ -1,21 +1,30 @@
 #ifndef ACCELTOPOS_H
 #define ACCELTOPOS_H
 
+#include <Value.h>
+#include <Outputable.h>
+#include <Controller_Input.h>
+#include <Controller_I.h>
 
-#include <ControllerSystem.h>
-class AccelToPos : public ControllerSystem
+
+class AccelToPos : Outputable
 {
 public:
-	AccelToPos();
+	AccelToPos(double InitPos = 0.0, double InitVel = 0.0);
 
 
 
 
 
+	bool setInput(Value V) { return this->Input_.setInput(V); };
 
+	Value getOutputValue() { return this->VelToPos_.getOutputValue(); };
+	Unit getOutputUnit() { return this->VelToPos_.getOutputUnit(); };
 
 private:
-
+	Controller_Input Input_;
+	Controller_I AccelToVel_;
+	Controller_I VelToPos_;
 };
 
 #endif // ACCELTOPOS_H
