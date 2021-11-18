75cdb942220406ed9f25144588791176512c7340
MaagMich <maag.michael94@web.de>
2021-11-18 09:11:46 +0100

    wip added interfaces

--- Code/Application/Application/AccelToPos.cpp
+++ Code/Application/Application/AccelToPos.cpp
@@ -2,25 +2,10 @@
 
 
 AccelToPos::AccelToPos(double InitPos, double InitVel)
-<<<<<<< HEAD
-	: Outputable(Unit("m"))/*,
-	Input_(Unit("m/s²")),
-	AccelToVel_(Unit("m/s²"), Unit("m/s"), 1.0),
-	VelToPos_(Unit("m/s"), Unit("m"), 1.0)*/
-{
-	/*
-	this->AccelToVel_.setIntputAddr(&this->Input_);
-	// set Sum to InitVel
-
-	this->VelToPos_.setIntputAddr(&this->AccelToVel_);
-	// set Sum to InitPos
-	*/
-=======
 	: Outputable(Unit("m")),
 	Input_(Unit("m/s²")),
 	AccelToVel_(Unit("m/s²"), Unit("m/s"), 1.0, &Input_, InitVel),
 	VelToPos_(Unit("m/s"), Unit("m"), 1.0, &AccelToVel_, InitPos)
 {
->>>>>>> devel_app
 }
 
--- Code/Application/Application/AccelToPos.h
+++ Code/Application/Application/AccelToPos.h
@@ -1,36 +1,18 @@
 #ifndef ACCELTOPOS_H
 #define ACCELTOPOS_H
 
-<<<<<<< HEAD
-#include "Value.h"
-#include "Outputable.h"
-#include "Controller_Input.h"
-#include "Controller_I.h"
-=======
+#include <Unit.h>
 #include <Value.h>
 #include <Outputable.h>
 #include <Controller_Input.h>
 #include <Controller_I.h>
->>>>>>> devel_app
 
 
 class AccelToPos : Outputable
 {
 public:
 	AccelToPos(double InitPos = 0.0, double InitVel = 0.0);
-<<<<<<< HEAD
 
-	/*
-=======
-<<<<<<< Updated upstream
-
-
-
-
-=======
->>>>>>> Stashed changes
-
->>>>>>> devel_app
 	bool setInput(Value V) { return this->Input_.setInput(V); };
 
 	Value getOutputValue() { return this->VelToPos_.getOutputValue(); };
@@ -40,10 +22,6 @@ private:
 	Controller_Input Input_;
 	Controller_I AccelToVel_;
 	Controller_I VelToPos_;
-<<<<<<< HEAD
-	*/
-=======
->>>>>>> devel_app
 };
 
 #endif // ACCELTOPOS_H
