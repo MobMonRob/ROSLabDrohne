a05a8804abc3e1600018b7011f38fc19c0da6495
MaagMich <maag.michael94@web.de>
2021-11-18 11:26:37 +0100

    merging...

--- Code/Application/Application/AccelToPos.h
+++ Code/Application/Application/AccelToPos.h
@@ -1,10 +1,10 @@
 #ifndef ACCELTOPOS_H
 #define ACCELTOPOS_H
 
-#include "Value.h"
-#include "Outputable.h"
-#include "Controller_Input.h"
-#include "Controller_I.h"
+#include <Value.h>
+#include <Outputable.h>
+#include <Controller_Input.h>
+#include <Controller_I.h>
 
 
 class AccelToPos : Outputable
@@ -18,11 +18,17 @@ public:
 	Value getOutputValue() { return this->VelToPos_.getOutputValue(); };
 	Unit getOutputUnit() { return this->VelToPos_.getOutputUnit(); };
 
+
+
+	bool setInput(Value V) { return this->Input_.setInput(V); };
+
+	Value getOutputValue() { return this->VelToPos_.getOutputValue(); };
+	Unit getOutputUnit() { return this->VelToPos_.getOutputUnit(); };
+
 private:
 	Controller_Input Input_;
 	Controller_I AccelToVel_;
 	Controller_I VelToPos_;
-	*/
 };
 
 #endif // ACCELTOPOS_H
