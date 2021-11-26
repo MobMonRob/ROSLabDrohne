85ced05a9db48c6bb4c779f921d97949867c4f86
MaagMich <maag.michael94@web.de>
2021-11-26 12:17:49 +0100

    wip

--- Code/PosControl/include/PosControl/State.h
+++ Code/PosControl/include/PosControl/State.h
@@ -4,23 +4,27 @@
 
 
 
-
-
+#include "PosControl/Timestamp.h"
+#include "PosControl/Vector3D.h"
 
 class State
 {
 public:
-	State();
+	State(Vector3D Translative, Vector3D Angular, Timestamp Time);
+
+
+
+
+
+
 
 
 
 
 private:
-
-
-
-
-
+	Vector3D Translative_;
+	Vector3D Angular_;
+	Timestamp Time_;
 };
 
 #endif // STATE_H
