9b5526dca657cd906d5f6a8a8261cb1427585ef2
MaagMich <maag.michael94@web.de>
2022-01-11 12:11:46 +0100

    wip added ROS and catkin files

---
+++ Code/keyboard/src/KeyNode.cpp
@@ -0,0 +1,16 @@
+#include <ros/ros.h>
+
+#include <keyboard/KeyReader.h>
+
+
+int main(int argc, char** argv)
+{
+	ros::init(argc, argv, "KeyReader");
+	KeyReader Reader;
+	
+	Reader.readingLoop();
+	
+	return 0;
+}
+
+
